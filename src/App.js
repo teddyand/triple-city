456a5ec6dffedb44ee90c582837370b7f527d904
teddyand <davidchen1977@live.com>
2024-08-18 17:06:00 +0800

    24 Aug 18th pm

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to David's mock life.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://davidit.top"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          David learns to mock
         </a>
       </header>
     </div>
